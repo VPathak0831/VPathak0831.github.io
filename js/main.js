425564f4d4a4bdb18b36367042a0f339ed634ff6
Vedant Pathak <vedant@Vedants-MacBook-Pro.local>
2017-02-27 13:13:18 -0600

    changed ajax call

--- js/main.js
+++ js/main.js
@@ -13,14 +13,11 @@ $('button').on('click',function(){
 	var content = $('input').val();
 	var tweet = new Tweet(content,Date());
 	var outputdata = JSON.stringify(tweet);
-    $.ajax
-    ({
-    type: "POST",
-    dataType : 'json',
-    url: 'https://VPathak0831.github.io/data/data.json',
-    data: outputdata,
-    success: function () {alert("Thanks!"); },
-    failure: function() {alert("Error!");}
+    $.ajax({
+	    type: "POST",
+	    dataType: 'json',
+	    url: 'https://VPathak0831.github.io/data/data.json',
+	    data: {outputdata},
     });
 	$('#posts').append('<div class="post"><p>'+tweet.message+'</p></div>');
 });
