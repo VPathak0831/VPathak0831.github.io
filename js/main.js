273dafc424b0eb25d8d1efe7beb682c4ace88c80
Vedant Pathak <vedant@Vedants-MacBook-Pro.local>
2017-02-27 17:14:09 -0600

    added pull functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,20 @@
+var spreadsheetUrl = 'https://docs.google.com/spreadsheets/d/1Edsp6Tc6rtA3EQeJAWdz7Jb0PHpJTc1m_cPjezs89-E/pubhtml';
+var spreadsheetID = '1Edsp6Tc6rtA3EQeJAWdz7Jb0PHpJTc1m_cPjezs89-E'
+var worksheetNumber = 'od6'
+
+$('document').ready(function(){
+	$.getJSON(spreadsheetUrl,function(data){
+		entries = data.feed.entry;
+		var sheet = [];
+		for(var i = 1; i < entries.length; i++) {
+			sheet[i-1] = {
+				"messages" : entries[i].gsx$post.$t,
+				"date" : entries[i].gsx$date.$t 
+			}
+		}
+	});
+});
+
 function Tweet(message,postTime){
 	this.message = message;
 	this.postTime = postTime;
@@ -14,7 +31,7 @@ $('button').on('click',function(){
 	var tweet = new Tweet(content,Date());
     $.ajax({
 	    type: "POST",
-	    dataType: 'xml',
+	    dataType: 'json',
 	    url: 'https://docs.google.com/forms/d/e/1FAIpQLSd0X3SSJcvLnRI_A8Uh9Chbd-SvnNIhXGRVF6x8O861bn7hXA/formResponse',
 	    data: {"entry.1507894826":tweet.message,"entry.997903729":tweet.postTime},
     });
